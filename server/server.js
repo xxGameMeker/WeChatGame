6f12415cdfc64ad2fcde50650bbcdea9a46517c5
gx774727341 <38370061+gx774727341@users.noreply.github.com>
2018-04-15 12:02:32 +0800

    123

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ io.on('connection', function(socket){
 
 // 	console.log('this is connect',socket.id);
 
-// 	socket.emit('connected123', 'connect');
+// 	socket.emit('connected123', 'connect');123
 // });
 
 
